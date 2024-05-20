b0e72f49ff7a9a3a36fa5707452a8eca4a92ddac
ShreeKalyaaneB <shreekalyaane@gmail.com>
2024-05-20 18:09:03 +0530

    error resolved

--- blogio/pages/auth/signin.js
+++ blogio/pages/auth/signin.js
@@ -92,12 +92,12 @@ export default function SignIn() {
               )}
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
-                <a
+                <Link
                   href="/auth/register"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Sign up
-                </a>
+                </Link>
               </p>
             </form>
           </div>
